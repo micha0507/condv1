7bba4e2a40658172f5f8a3b98c46fc5ab6b149c0
lgfr03 <lgfr03@gmail.com>
2026-02-12 02:06:13 -0400

    correcion con info condominio precargada en registro condominio

--- register_condominio.php
+++ register_condominio.php
@@ -23,16 +23,33 @@
             <img src="/img/icono_condo.jpg">
             <h2 class="title">REGISTRARSE</h2>
             <?php
-
-            // Establecer conexión con la base de datos
-
+            // 1. Conexión y Lógica de Precarga
             $conexion = new mysqli("localhost", "root", "", "condominio");
             $conexion->set_charset("utf8");
+
+            // Variables para controlar el estado
+            $existe_condominio = false;
+            $rif_defecto = "";
+            $nombre_condo_defecto = "";
+            $ubicacion_defecto = "";
+
+            // Consultamos si ya existe al menos un administrador registrado
+            // NOTA: Asegúrate que los nombres de columnas (rif_admin, etc) sean iguales en tu Base de Datos
+            $sql = "SELECT rif_admin, nombre_condominio, direccion_condominio FROM administrador LIMIT 1";
+            $resultado = $conexion->query($sql);
+
+            if ($resultado && $resultado->num_rows > 0) {
+               $fila = $resultado->fetch_assoc();
+               $existe_condominio = true;
+               $rif_defecto = $fila['rif_admin'];
+               $nombre_condo_defecto = $fila['nombre_condominio'];
+               $ubicacion_defecto = $fila['direccion_condominio'];
+            }
+
             include "controlador/controlador_register.php";
-            include "modelo/conexion.php";
+            // include "modelo/conexion.php"; // Comenté esto porque ya abriste conexión arriba y podría causar conflicto
             ?>
 
-            <!-- Campos de entrada para el registro del condominio -->
             <div class="input-div one">
                <div class="i">
                   <i class="fas fa-user"></i>
@@ -55,37 +72,7 @@
 
             <div class="input-div one">
                <div class="i">
-                  <i class="fas fa-user"></i>
-               </div>
-               <div class="div">
-                  <h5>RIF del Condominio</h5>
-                  <input type="text" class="input" name="rif_admin" id="rif_admin">
-               </div>
-            </div>
-
-            <div class="input-div one">
-               <div class="i">
-                  <i class="fas fa-user"></i>
-               </div>
-               <div class="div">
-                  <h5>Nombre del Condominio</h5>
-                  <input type="text" class="input" name="nombre_condominio" id="nombre_condominio">
-               </div>
-            </div>
-
-            <div class="input-div one">
-               <div class="i">
-                  <i class="fas fa-user"></i>
-               </div>
-               <div class="div">
-                  <h5>Ubicacion del Condominio</h5>
-                  <input type="text" class="input" name="direccion_condominio" id="direccion_condominio">
-               </div>
-            </div>
-
-            <div class="input-div one">
-               <div class="i">
-                  <i class="fas fa-user"></i>
+                  <i class="fas fa-envelope"></i>
                </div>
                <div class="div">
                   <h5>Correo Electronico</h5>
@@ -106,10 +93,47 @@
                <div class="fas fa-eye verPassword" onclick="vista()" id="verPassword"></div>
             </div>
 
-            <div class="text-center">
-               <a class="font-italic isai5" href="login.php">¿Ya tienes una cuenta Creada?</a>
-            </div>
-            <input class="btn" type="submit" value="REGISTRAR CONDOMINIO" name="btnregistrar">
+            <div class="input-div one <?php if ($existe_condominio) echo 'focus'; ?>">
+               <div class="i">
+                  <i class="fas fa-id-card"></i>
+               </div>
+               <div>
+                  <div class="div">
+                     <h5>RIF del Condominio</h5>
+                     <input type="text" class="input" name="rif_admin" id="rif_admin"
+                        value="<?php echo $rif_defecto; ?>"
+                        <?php if ($existe_condominio) echo 'readonly'; ?>>
+                  </div>
+               </div>
+
+               <div class="input-div one <?php if ($existe_condominio) echo 'focus'; ?>">
+                  <div class="i">
+                     <i class="fas fa-building"></i>
+                  </div>
+                  <div class="div">
+                     <h5>Nombre del Condominio</h5>
+                     <input type="text" class="input" name="nombre_condominio" id="nombre_condominio"
+                        value="<?php echo $nombre_condo_defecto; ?>"
+                        <?php if ($existe_condominio) echo 'readonly'; ?>>
+                  </div>
+               </div>
+
+               <div class="input-div one <?php if ($existe_condominio) echo 'focus'; ?>">
+                  <div class="i">
+                     <i class="fas fa-map-marker-alt"></i>
+                  </div>
+                  <div class="div">
+                     <h5>Ubicacion del Condominio</h5>
+                     <input type="text" class="input" name="direccion_condominio" id="direccion_condominio"
+                        value="<?php echo $ubicacion_defecto; ?>"
+                        <?php if ($existe_condominio) echo 'readonly'; ?>>
+                  </div>
+               </div>
+
+               <div class="text-center">
+                  <a class="font-italic isai5" href="login.php">¿Ya tienes una cuenta Creada?</a>
+               </div>
+               <input class="btn" type="submit" value="REGISTRAR CONDOMINIO" name="btnregistrar">
          </form>
       </div>
    </div>
